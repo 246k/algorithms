1f28bc626a365defc4b25493773eefe937c762e3
246 <katekraynak@gmail.com>
2021-04-19 16:20:51 -0400

    updated manachers

--- manachersAlgorithm.py
+++ manachersAlgorithm.py
@@ -1,50 +1,33 @@
 def manachersAlgorithm(word):
     maxLength = 0
-    newInputString = ""
-    outputString = ""
-
-    newInputString = word
-    # append last character
-    newInputString += word[-1]
-
-    # we will store the starting and ending of previous furthest ending palindromic
-    # substring
     l, r = 0, 0
 
-    # length[i] shows the length of palindromic substring with center i
-    length = [1 for i in range(len(newInputString))]
+    longestPalindromeArray = [1 for i in range(len(word))]
 
-    # for each character in new_string find corresponding palindromic string
-    for i in range(len(newInputString)):
-        k = 1 if i > r else min(length[l + r - i] // 2, r - i + 1)
+    for i in range(len(word)):
+        k = 1 if i > r else min(longestPalindromeArray[l + r - i] // 2, r - i + 1)
         while (
             i - k >= 0
-            and i + k < len(newInputString)
-            and newInputString[k + i] == newInputString[i - k]
+            and i + k < len(word)
+            and word[k + i] == word[i - k]
         ):
             k += 1
 
-        length[i] = 2 * k - 1
+        longestPalindromeArray[i] = 2 * k - 1
 
-        # does this string is ending after the previously explored end (that is r) ?
-        # if yes the update the new r to the last index of this
         if i + k - 1 > r:
             l = i - k + 1
             r = i + k - 1
 
-        # update max_length and start position
-        if maxLength < length[i]:
-            maxLength = length[i]
+        if maxLength < longestPalindromeArray[i]:
+            maxLength = longestPalindromeArray[i]
             start = i
 
-    # create that string
-    s = newInputString[start - maxLength // 2 : start + maxLength // 2 + 1]
-    for i in s:
-        outputString += i
-    if len(outputString) > 1:
-        return outputString
+    result = word[start - maxLength // 2 : start + maxLength // 2 + 1]
+    if len(result) > 1:
+        return result
     else: 
         return "No palindromic substrings found." 
 
 
-print(manachersAlgorithm("racecar"))
+print(manachersAlgorithm("racecarpdfg"))
